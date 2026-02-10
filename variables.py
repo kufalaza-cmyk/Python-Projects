5ca83d4f8f50e87cb4c226a7333d3f470c818a9e
kufalaza-cmyk <kufalaza@gmail.com>
2026-02-10 15:41:48 +0200

    Add variable demonstration in variables.py
    Demonstrate variable usage in Python with examples.

---
+++ variables.py
@@ -0,0 +1,24 @@
+below is a demonistration of how variables work in python
+
+first_name = 'Lazarus'
+last_name  = 'Nyirongo'
+country  = 'Malawi'
+city = 'Blantyre'
+age = 23
+is_married = True
+skills = ['HTML', 'css', 'JS', 'React', 'Python']
+person_info = {
+   'firstname':'Asabeneh',
+   'lastname':'Yetayeh',
+   'country':'Finland',
+   'city':'Helsinki'
+   }
+
+print('First name:', first_name)
+print('First name lenght:', len(first_name))
+print('country:', country)
+print('country lenght:', len(country))
+print('age:', age)
+print('Married:', is_married)
+print('Skills:', skills)
+print('Person information:', person_info)
