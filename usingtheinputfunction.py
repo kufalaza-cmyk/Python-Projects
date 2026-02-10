40221dd3b7a94083a0b70270b42fde588304b04a
kufalaza-cmyk <kufalaza@gmail.com>
2026-02-10 15:46:52 +0200

    Add input function to collect user information

---
+++ usingtheinputfunction.py
@@ -0,0 +1,9 @@
+first_name = input('What is your name: ')
+last_name = input('What is your last name: ')
+age = input('How old are you? ')
+country = input('What country are you from? ')
+
+print(first_name)
+print(last_name)
+print(age)
+print(country)
