c2cd3d0671d7537103b70e5a458e17b872d78edc
kufalaza-cmyk <kufalaza@gmail.com>
2026-02-10 16:12:07 +0200

    Add user input for personal information

---
+++ variablespractice.py
@@ -0,0 +1,12 @@
+first_name = input('Enter name: ')
+last_name = input('Enter last name? ')
+full_name = first_name + ' ' + last_name
+country = input('Country of origin: ')
+age = input('age: ')
+year = input('What year is it: ')
+
+print('first_name', first_name)
+print('last_name', last_name)
+print('full_name', full_name)
+print('age', age)
+print('year', year)
