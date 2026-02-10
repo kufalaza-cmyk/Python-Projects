ac40d434bec2d062872675ac4ae695787e20d384
kufalaza-cmyk <kufalaza@gmail.com>
2026-02-10 15:16:36 +0200

    Add Day 1 exercises for Python basics

---
+++ Day1of30daysofPython.py
@@ -0,0 +1,19 @@
+30 days of python
+
+print("Hello World!")
+
+print(2 + 3)   #addition
+print(3 - 1)   #subtraction
+print(2 * 3)   #multiplication
+print(3 / 2)   #division
+print(3 ** 2)  #exponential
+print(3 % 2)   #modulus
+print(3 // 2)  #Floor division operator 
+
+print(type(10))  #this is an interger int 
+print(type(3.14))  #this is a float
+print(type(1 + 3j))  #complex
+print(type('Lazarus')) #itsaString
+print(type([1, 2, 3]))  #thisisalist
+print(type({'name': 'Lazarus'}))  #thisisadictionary
+print(type({9.8, 3.14, 2.7}))  #set/Tuple
